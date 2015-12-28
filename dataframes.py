862c8959e0d41fc8cac31cfb78c266b40986a187
arose13 <anthony_rose12@hotmail.com>
2015-12-28 15:13:46 -0500

    sorting arrays by a particular column

--- dataframes.py
+++ dataframes.py
@@ -1,14 +1,15 @@
 # Adding Columns to a DataFrame
 # Also check out this StackOverFlow answer...
 # http://stackoverflow.com/questions/13413590/how-to-drop-rows-of-pandas-dataframe-whose-value-of-certain-column-is-nan
+import numpy as np
 import pandas as pd
 
 # Prep Values
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = [7, 8, 9]
-d = [10, None, 12]
-e = [13, 14, 15]
+a = [1, 2, 3, 0]
+b = [4, 5, 6, -3]
+c = [7, 8, 9, -6]
+d = [10, None, 12, -12]
+e = [13, 14, 15, -15]
 
 # Make DataFrame with the data
 df = pd.DataFrame()
@@ -28,8 +29,14 @@ print(f)
 
 # Remove row with NaN value
 g = df.dropna().values
+g = np.array(g)
 print('\nNumPy array with the NaN rows removed')
 print(g)
+
+# Let's just sort g but the numbers in the first row just to see what it says
+g_sort = g[g[:, 0].argsort()]
+print('\nSorted by first column')
+print(g_sort)
 
 h = g.T  # Transpose. We're going to select the first 60% of rows
 print('\nNumPy array Transposed')
@@ -39,3 +46,7 @@ print(h)
 h60 = h[0:(round(0.6 * len(h)))]
 print('\nFirst 60% of the transposed rows')
 print(h60)
+
+h40 = h[(round(0.6 * len(h))): len(h)]
+print('\nLast 40% of the transposed rows')
+print(h40)
