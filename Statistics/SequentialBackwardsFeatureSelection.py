06a601c60635492c71b369e502b25168f1b3f9e6
arose13 <anthony_rose12@hotmail.com>
2016-01-27 14:11:20 -0500

    SBFS transforms features to the best features

--- Statistics/SequentialBackwardsFeatureSelection.py
+++ Statistics/SequentialBackwardsFeatureSelection.py
@@ -2,7 +2,6 @@ from itertools import combinations
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as graph
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 
 __author__ = 'Anthony Rose'
@@ -60,11 +59,30 @@ class SBFS:
 
         return self
 
+    def best_features(self):
+        """
+        Returns the minimum number of features with the highest score.
+        :return:
+        """
+        best_subset = None
+        best_score = 0
+
+        # TODO There must be a more python way of doing this
+        for score, subset in zip(self.scores_, self.subsets_):
+            if best_subset is None:
+                best_subset = subset
+
+            if score >= best_score and len(subset) < len(best_subset):
+                best_score = score
+                best_subset = subset
+
+        return best_subset
+
     def transform(self, x):
         """
         This will best feature found via back selection
         """
-        return x[:, self.indices_]
+        return x[:, self.best_features()]
 
     def calc_score(self, x_train, y_train, x_test, y_test, indices):
         self.model.fit(x_train[:, indices], y_train)
@@ -76,7 +94,6 @@ class SBFS:
         k_feature = [len(k) for k in self.subsets_]
         # Graph Accuracy vs Number of features
         graph.plot(k_feature, self.scores_, marker='o')
-        graph.title('Accuracy vs Features Used')
         graph.ylabel('Accuracy')
         graph.xlabel('Number of Features')
         graph.show()
@@ -86,16 +103,40 @@ if __name__ == '__main__':
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn.datasets import load_breast_cancer
     from sklearn.preprocessing import StandardScaler
+    from sklearn.cross_validation import train_test_split
 
     # Load
     sns.set()
     data = load_breast_cancer()
+    x = StandardScaler().fit_transform(data.data)
+    y = data.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
 
     # Run Feature Selection
-    classifier = KNeighborsClassifier(n_neighbors=2)
-    sbfs = SBFS(classifier, k_features=1)
+    model = KNeighborsClassifier(n_neighbors=2)
+    sbfs = SBFS(model, k_features=1)
     sbfs.fit(
-        StandardScaler().fit_transform(data.data),
-        data.target
+        x_train,
+        y_train
     )
-    sbfs.graph_performance()
+
+    # Observe Results
+    best_features = sbfs.best_features()
+    x_best_train, x_best_test = sbfs.transform(x_train), sbfs.transform(x_test)
+
+    print(best_features)
+    print('X', x_test.shape)
+    print('X Best', x_best_test.shape)
+
+    # Compare Best Features vs All Features Results
+    # ALL model
+    model.fit(x_train, y_train)
+    print('\nALL MODEL')
+    print('Training Accuracy {}'.format(model.score(x_train, y_train)))
+    print('Test Accuracy {}'.format(model.score(x_test, y_test)))
+
+    # BEST FEATURES model
+    model.fit(x_best_train, y_train)
+    print('\nBEST MODEL')
+    print('Training Accuracy {}'.format(model.score(x_best_train, y_train)))
+    print('Test Accuracy {}'.format(model.score(x_best_test, y_test)))
