818d2a52be85eaa81c6709c71d28dd89d3940232
arose13 <anthony_rose12@hotmail.com>
2016-01-28 11:23:46 -0500

    testing if in and sleeping with warmer rule random string guessing

---
+++ Randos/randomGuessing.py
@@ -0,0 +1,30 @@
+import string
+import random
+import time
+
+possibleCharacters = string.ascii_lowercase + string.digits + string.ascii_uppercase + ' .,!?;:<>[]{}~!@#$%^&*()_+-='
+
+target = input("Enter your target text: ")
+attemptThis = ''.join(random.choice(possibleCharacters) for i in range(len(target)))
+attemptNext = ''
+
+completed = False
+
+generation = 0
+
+while not completed:
+    print(attemptThis)
+    attemptNext = ''
+    completed = True
+    for i in range(len(target)):
+        if attemptThis[i] != target[i]:
+            completed = False
+            attemptNext += random.choice(possibleCharacters)
+        else:
+            attemptNext += target[i]
+    generation += 1
+    attemptThis = attemptNext
+    if 'sleep' in target:
+        time.sleep(0.25)
+
+print("Target matched! That took " + str(generation) + " generation(s)")
