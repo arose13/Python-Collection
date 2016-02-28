3667e86b8eab47a694566e88288f96a3bc49fa92
arose13 <anthony_rose12@hotmail.com>
2016-02-28 11:51:06 -0500

    Daily weight data. Notebook.

--- Randos/np.py
+++ Randos/np.py
@@ -6,8 +6,11 @@ b = np.array([5, 4, 9, 5, 4, 9])
 
 c = b / a
 
-print(c)  # Expected [5, 2, 3, 1.25, 0.8, 1.5]
+print('input array', c)  # Expected [5, 2, 3, 1.25, 0.8, 1.5]
 
 # Select only the items that fulfil a requirement
 d = c[np.where(c >= 2)]
-print(d)
+print('where greater than 2', d)
+
+e = c[np.where(c >= np.mean(c))]
+print('where greater than average', e)
